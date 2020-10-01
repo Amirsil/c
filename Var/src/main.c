951733807828301b7c4ca0e2ee6ba0fe2dd01467
unknown <amirsil2002@gmail.com>
2020-10-02 02:28:12 +0300

    a couple more changes in main.c for unit testing

--- Var/src/main.c
+++ Var/src/main.c
@@ -1,15 +1,14 @@
 #include "var.h"
 
 int main(){
-	Var dynamics[20] = {0};
+	Var dynamics[10] = {0};
 	size_t length = sizeof(dynamics)/sizeof(*dynamics);
-	for (int i = length + 'A'; i --> 'A';) char_var(i, dynamics + i - 'A');
 
+	for (int i = length + 'A'; i --> 'A';) char_var(i, dynamics + i - 'A');
 	for (int i = length; i --> 0 ;) print_var(dynamics + i);
 
 	char str[80] = {0};
-	for (int i = length; i --> 0 ;)
-	{
+	for (int i = length; i --> 0 ;){
 		strcat(str, "A");
 		str_var(str, dynamics + i);
 	}
@@ -17,8 +16,8 @@ int main(){
 	for (int i = length; i --> 0 ;) print_var(dynamics + i);
 
 	for (int i = length; i --> 0 ;) free_var(dynamics + i);
-
-
+	
+	puts("");
 
 	Var dynamic = {0};
 	str_var("foo", &dynamic);
@@ -33,5 +32,7 @@ int main(){
 
 	char_var(0x41, &dynamic);
 	print_var(&dynamic);
+	free_var(&dynamic);
+	
 	return 0;
 }
