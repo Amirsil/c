252883852447d208e09d9eb217c48f6dabfef554
unknown <amirsil2002@gmail.com>
2020-10-01 19:28:20 +0300

    now the struct is initialized on the heap, and the only thing left on the stack is a lonely Var pointer

--- Var/src/main.c
+++ Var/src/main.c
@@ -1,23 +1,23 @@
 #include "var.h"
 
 int main(){
-	Var dynamic = {};
+	Var *dynamic = NULL;
 	
 	str_var("foo", &dynamic);
-	print_var(&dynamic);
+	print_var(dynamic);
 
 	int_var(0xdeadbeef, &dynamic);
-	print_var(&dynamic);
+	print_var(dynamic);
 
 	uint_var(-1, &dynamic);
-	print_var(&dynamic);
+	print_var(dynamic);
 
 	char_var(0x41, &dynamic);
-	print_var(&dynamic);
+	print_var(dynamic);
 
 
 	free_var(&dynamic);
-	print_var(&dynamic);
+	print_var(dynamic);
 
 	return 0;
 }
